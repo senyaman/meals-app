2a19866330d8fa799a290320754d8dc60426a3f7
Senya- Man <masenyagift0@gmail.com>
2022-07-12 17:28:16 +0200

    First commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
+
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+
+  @override
+  void didChangeDependencies() {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    categoryTitle = routeArgs['title'];
+    final categorId = routeArgs['id'];
+    displayedMeals = widget.availableMeals.where((meal) {
+      return meal.categories.contains(categorId);
+    }).toList();
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return MealItem(
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            duration: displayedMeals[index].duration,
+            complexity: displayedMeals[index].complexity,
+            affordability: displayedMeals[index].affordability,
+          );
+        },
+        itemCount: displayedMeals.length,
+      ),
+    );
+  }
+}
